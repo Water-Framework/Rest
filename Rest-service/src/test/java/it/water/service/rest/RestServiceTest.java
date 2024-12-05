7768f7a6f8796b60c40f58207ceba24875a1a7a1
Aristide <aristide.cittadino@acsoftware.it>
2024-12-05 07:39:23 +0100

    fix tests

--- Rest-service/src/test/java/it/water/service/rest/RestServiceTest.java
+++ Rest-service/src/test/java/it/water/service/rest/RestServiceTest.java
@@ -16,20 +16,35 @@
 
 package it.water.service.rest;
 
+import it.water.core.api.service.Service;
+import it.water.core.interceptors.annotations.Inject;
 import it.water.core.model.BaseError;
 import it.water.core.model.exceptions.ValidationException;
 import it.water.core.model.validation.ValidationError;
 import it.water.core.permission.exceptions.UnauthorizedException;
+import it.water.core.testing.utils.junit.WaterTestExtension;
 import it.water.repository.entity.model.exceptions.DuplicateEntityException;
 import it.water.repository.entity.model.exceptions.EntityNotFound;
 import it.water.repository.entity.model.exceptions.NoResultException;
+import it.water.service.rest.api.options.RestOptions;
+import lombok.Setter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 
-class RestServiceTest {
+@ExtendWith({MockitoExtension.class, WaterTestExtension.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RestServiceTest implements Service {
+
+    @Inject
+    @Setter
+    private RestOptions restOptions;
+
     @Test
     void testGenericExceptionMapper() {
         GenericExceptionMapperProvider exceptionMapperProvider = new GenericExceptionMapperProvider();
@@ -52,6 +67,16 @@ class RestServiceTest {
         Assertions.assertEquals(500,error.getStatusCode());
         error = exceptionMapperProvider.handleException(new IllegalArgumentException());
         Assertions.assertEquals(500,error.getStatusCode());
+    }
+
+    @Test
+    void testRestOptions(){
+        Assertions.assertNotNull(restOptions.securityOptions());
+        Assertions.assertNotNull(restOptions.restRootContext());
+        Assertions.assertNotNull(restOptions.servicesUrl());
+        Assertions.assertNotNull(restOptions.frontendUrl());
+        Assertions.assertNotNull(restOptions.uploadFolderPath());
+        Assertions.assertTrue(restOptions.uploadMaxFileSize() > 0);
     }
 
 }
